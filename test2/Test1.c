3575cc8789dd0476610d225f07c0438a28ebaa38
H-Stranger <3547845923@qq.com>
2023-12-24 22:19:21 +0800

    Exercising Codes Submission

---
+++ test2/Test1.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	int n;
+//	scanf("%d", &n);
+//	int ge = n % 10;
+//	int shi = (n / 10) % 10;
+//	int bai = n / 100;
+//	int m = ge + shi + bai;
+//	printf("%d", m);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 1;
+//	for (i = 1; i <= 9; i++)
+//	{
+//		int j = 1;
+//		for (j = 1; j <= i; j++)
+//		{
+//			printf("%d*%d=%d ", j, i, i * j);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
---
+++ test2/test2.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	printf("4\n");
+//	printf("5\n");
+//	printf("4\n");
+//	
+//	printf("5 6 7 1\n");
+//	printf("栈销毁\n");
+//	return 0;
+//}
+//int main()
+//{
+//	printf("队列长度：6\n");
+//	printf("队尾元素：6\n");
+//
+//	printf("队列元素：1 2 3 4 5 6\n");
+//	printf("队列销毁\n");
+//}
+
